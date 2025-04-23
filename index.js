375856f9f6e7e1d4ed5f1c3ff202c49ed71b6f9e
Ahsan Adil <muhammadmahsanadil@gmail.com>
2025-04-24 00:56:34 +0300

    my chatbot llama model is now working perfectly

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 
 // ------------------  Llama 4 Model  ----------------------
-
-import { Groq } from 'groq-sdk';
-import dotenv from "dotenv"
-
-dotenv.config();
-const groq = new Groq({
-    apiKey: process.env.OPEN_API_KEY
-});
-
+import groq from "./config/groq.js";
+import readlineSync from "readline-sync";
+import colors from "colors"
 
 async function main() {
-    const chatCompletion = await groq.chat.completions.create({
-        "messages": [
-            { role: 'user', content: 'What is the Capital of Pakistan?' }
-        ],
-        "model": "meta-llama/llama-4-scout-17b-16e-instruct",
-        "temperature": 1,
-        "max_completion_tokens": 1024,
-        "top_p": 1,
-        "stream": true,
-        "stop": null
-    });
 
-    let fullResponse = '';
+    console.log(colors.bold.green("Hey it's Ahsan Adil. Welcome to the chatbot program!"))
+    console.log(colors.bold.green("You can start chatting or type 'exit' to exit the chatbot."))
 
-    for await (const chunk of chatCompletion) {
-        const chunkContent = process.stdout.write(chunk?.choices[0]?.delta?.content || '');
-        if (typeof chunkContent === 'string' && chunkContent && !chunkContent.includes("true")) {
-            fullResponse += chunkContent
+    const chatHistory = []
+
+    while (true) {
+        const userInput = readlineSync.question('\n' + colors.bold.yellow("You: "))
+        try {
+            const messages = chatHistory.map(([role, content]) => ({ role, content }))
+
+            messages.push({ role: 'user', content: userInput })
+
+            const chatCompletion = await groq.chat.completions.create({
+                "messages": messages,
+                "model": "meta-llama/llama-4-scout-17b-16e-instruct",
+                "temperature": 1,
+                "max_completion_tokens": 1024,
+                "top_p": 1,
+                "stream": true,
+                "stop": null
+            });
+
+            let fullResponse = '';
+            console.log(colors.green('Bot: ') + fullResponse)
+
+            for await (const chunk of chatCompletion) {
+                const chunkContent = process.stdout.write(chunk?.choices[0]?.delta?.content || '');
+                if (typeof chunkContent === 'string' && chunkContent && !chunkContent.includes("true")) {
+                    fullResponse += chunkContent
+                }
+            }
+
+            if (userInput.toLowerCase() === "exit") {
+                console.log(fullResponse)
+                return
+            }
+            chatHistory.push(['user', userInput])
+            chatHistory.push(['assistant', fullResponse])
+
+        } catch (error) {
+            console.error(colors.red(error))
         }
     }
 
-    console.log(fullResponse)
+
+
+
 }
 
 main();
